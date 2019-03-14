d2da1e6e31f4aceed007fa47dfd2fb4bd57ba5ea
Matthew Elder <sseses@gmail.com>
2019-03-14 13:03:31 -0700

    create readme md with example

---
+++ parsers/rs/README.md
@@ -0,0 +1,21 @@
+### example
+````
+➜ ./parser 
+EWPRequest {
+    version: "0.1",
+    command: "PING",
+    compression: "none",
+    response_compression: [
+        "none"
+    ],
+    head_only_indicator: false,
+    headers: [],
+    body: [
+        49,
+        50,
+        51,
+        52,
+        53
+    ]
+}
+```
--- parsers/rs/parser.rs
+++ parsers/rs/parser.rs
@@ -39,28 +39,3 @@ fn main() {
     let req = EWPRequest::parse(EXAMPLE_PING_REQ).expect("parse failed");
     println!("{:#?}", req)
 }
-/*
-function parse(req) {
-    let r = reqLine.split(' ')
-    let headersLen = parseInt(r[5])
-    let bodyLen = parseInt(r[6])
-    let headers = payload.slice(0, headersLen)
-    let body = payload.slice(headersLen, bodyLen)
-
-    return {
-        proto: r[0],
-        version: r[1],
-        command: r[2],
-        compression: r[3],
-        responseCompression: r[4].split(','),
-        headOnlyIndicator: r[7] === 'H',
-        headers,
-        body
-    }
-}
-
-function test() {
-    console.log(parse(example_ping_req))
-}
-test()
-*/
