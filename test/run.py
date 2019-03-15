422be4f6bdfa66e5ad24a463c6f46133db314365
Matthew Elder <sseses@gmail.com>
2019-03-15 01:13:14 -0700

    rust test compat almost done

--- test/run.py
+++ test/run.py
@@ -15,36 +15,43 @@ class DynamicTest(unittest.TestCase):
 def quote(token):
     return '"' + token + '"'
 
+def bracket(token):
+    return '[' + token + ']'
+
 def load_yaml_file(path):
     with open(path, 'r') as stream:
         return yaml.load(stream)
 
 def generate_rust_field_line_value(name, r):
     switch = {
-        'head-only': lambda r=r: str(r.get('head-only', False)),
+        'head-only': lambda r=r: str(r.get('head-only', False)).lower(),
         'headers': lambda r=r: r.get('headers', '[]'),
         'body': lambda r=r: r.get('body', '[]'),
-        'response-compression': lambda r=r: ",".join(r['response-compression']),
+        'response-compression': lambda r=r: bracket(", ".join(map(lambda rc: quote(rc), r['response-compression']))),
         'version': lambda r=r: VERSION,
     }
-    default_fun = lambda r=r, name=name: r.get(name)
+    default_fun = lambda r=r, name=name: quote(r[name])
     val = switch.get(name, default_fun)()
 
     if val == None:
         raise ValueError('could not generate rust field line for {0}'.format(name))
     else:
-        return quote(val)
+        return val
 
 
 
 def generate_rust_expected_pretty(r):
-    lines = [
+    parts= [
         'EWPRequest {',
     ]
-    lines.extend(map(lambda f, r=r: '    ' + f + ': ' + generate_rust_field_line_value(f, r), FIELDS))
-    lines.append('}')
+    parts.append(
+        ", ".join(map(lambda f, r=r:
+            f.replace('head-only', 'head-only-indicator').replace('-', '_') + ': ' + generate_rust_field_line_value(f, r), FIELDS
+        ))
+    )
+    parts.append('}')
 
-    return "\n".join(lines)
+    return " ".join(parts)
 
 '''
 EWPRequest {
@@ -72,19 +79,24 @@ if __name__ == '__main__':
         suite_name = re.sub('[ -]', '_', suite['suite']).lower()
 
         for request in suite['requests']:
-            print(generate_rust_expected_pretty(request))
-            desc = re.sub('[ -]', '_', request['desc']).lower()
-            test_func = lambda self, request=request: println(request)
+            actual = subprocess.check_output(["./parsers/rs/parser" ])
+            print(actual)
+            expected = generate_rust_expected_pretty(request)
 
+            desc = re.sub('[ -]', '_', request['desc']).lower()
+
+            test_func = lambda self, actual=actual, expected=expected: self.assertEqual(actual, expected)
             test_name = "_".join([ 'test', suite_name, 'request', desc ])
+
             setattr(DynamicTest, test_name, test_func)
 
         for request in suite['responses']:
             desc = re.sub('[ -]', '_', request['desc']).lower()
+
             test_func = lambda self, desc=desc : self.assertEqual(1, 2)
             test_name = "_".join([ 'test', suite_name, 'response', desc ])
+
             setattr(DynamicTest, test_name, test_func)
 
-    #subprocess.call(["./parsers/rs/parser" ])
     unittest.main(verbosity=3)
 
