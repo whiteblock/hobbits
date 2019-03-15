f708f25129c30addaaec261abf2049e87ba40fb8
Matthew Elder <sseses@gmail.com>
2019-03-15 00:34:00 -0700

    wip on generative parse pretty print output for rust

--- test/run.py
+++ test/run.py
@@ -3,13 +3,68 @@ import subprocess
 import unittest
 import yaml
 
+VERSION = '0.1'
+FIELDS = [
+    "version", "command", "compression", "response-compression",
+    "head-only", "headers", "body"
+]
+
 class DynamicTest(unittest.TestCase):
     longMessage = True
+
+def quote(token):
+    return '"' + token + '"'
 
 def load_yaml_file(path):
     with open(path, 'r') as stream:
         return yaml.load(stream)
 
+def generate_rust_field_line_value(name, r):
+    switch = {
+        'head-only': lambda r=r: str(r.get('head-only', False)),
+        'headers': lambda r=r: r.get('headers', '[]'),
+        'body': lambda r=r: r.get('body', '[]'),
+        'response-compression': lambda r=r: ",".join(r['response-compression']),
+        'version': lambda r=r: VERSION,
+    }
+    default_fun = lambda r=r, name=name: r.get(name)
+    val = switch.get(name, default_fun)()
+
+    if val == None:
+        raise ValueError('could not generate rust field line for {0}'.format(name))
+    else:
+        return quote(val)
+
+
+
+def generate_rust_expected_pretty(r):
+    lines = [
+        'EWPRequest {',
+    ]
+    lines.extend(map(lambda f, r=r: '    ' + f + ': ' + generate_rust_field_line_value(f, r), FIELDS))
+    lines.append('}')
+
+    return "\n".join(lines)
+
+'''
+EWPRequest {
+    version: "0.1",
+    command: "PING",
+    compression: "none",
+    response_compression: [
+        "none"
+    ],
+    head_only_indicator: false,
+    headers: [],
+    body: [
+        49,
+        50,
+        51,
+        52,
+        53
+    ]
+}
+'''
 if __name__ == '__main__':
     suites = load_yaml_file('test/tests.yaml')
 
@@ -17,6 +72,7 @@ if __name__ == '__main__':
         suite_name = re.sub('[ -]', '_', suite['suite']).lower()
 
         for request in suite['requests']:
+            print(generate_rust_expected_pretty(request))
             desc = re.sub('[ -]', '_', request['desc']).lower()
             test_func = lambda self, request=request: println(request)
 
@@ -29,5 +85,6 @@ if __name__ == '__main__':
             test_name = "_".join([ 'test', suite_name, 'response', desc ])
             setattr(DynamicTest, test_name, test_func)
 
+    #subprocess.call(["./parsers/rs/parser" ])
     unittest.main(verbosity=3)
-    subprocess.run([" parsers/rs/parser" ])
+
