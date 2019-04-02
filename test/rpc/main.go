56e36307c8d6ed8dd84e1b1aa83814c5ef47bda3
zscole <zak@whiteblock.io>
2019-04-02 13:36:21 -0700

    check port availability

--- test/rpc/main.go
+++ test/rpc/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/pflag"
 )
 
 var (
 	ip   string
-	port int
+	port string
 )
 
 func main() {
 	pflag.StringVarP(&ip, "ip", "i", "10.1.0.2", "ip address")
-	pflag.IntVarP(&port, "port", "p", 9000, "port")
+	pflag.StringVarP(&port, "port", "p", "9000", "port")
 
 	pflag.Parse()
 
 	pinger()
+	fmt.Println("Ping test passed. Testing port...")
+	porter()
+	fmt.Printf("Port test passed. Testing message delivery...", port)
 
 }
---
+++ test/rpc/port.go
@@ -0,0 +1,24 @@
+package main
+
+import "net"
+import "fmt"
+
+import "os"
+
+func porter() {
+	ln, err := net.Listen("tcp", ip+":"+port)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Test failed. Port %q: %s unavailable.", port, err)
+		os.Exit(1)
+	}
+
+	err = ln.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't close port %q: %s", port, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("TCP Port %q is available", port)
+	os.Exit(0)
+}
